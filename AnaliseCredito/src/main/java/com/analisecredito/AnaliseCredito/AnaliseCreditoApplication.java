d8b8ce0a6c376e66db73cc881c67ded898800fbf
Mauricio <maalvessilva01@gmail.com>
2024-12-09 15:11:50 -0300

    primeiro commit

---
+++ AnaliseCredito/src/main/java/com/analisecredito/AnaliseCredito/AnaliseCreditoApplication.java
@@ -0,0 +1,13 @@
+package com.analisecredito.AnaliseCredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnaliseCreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnaliseCreditoApplication.class, args);
+	}
+
+}
---
+++ AnaliseCredito/src/test/java/com/analisecredito/AnaliseCredito/AnaliseCreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.analisecredito.AnaliseCredito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnaliseCreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
